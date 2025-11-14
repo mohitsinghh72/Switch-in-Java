ee5a03d3d8f28ecc9b40d9366779d1979b22afe3
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-14 23:59:41 +0530

    Enhanced switch

--- SelectingFruit.java
+++ SelectingFruit.java
@@ -4,7 +4,7 @@ public class SelectingFruit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter fruit name : ");  // choose fruit name between apple,mango,Banana,Watermelon
-        String fruits = sc.next();
+        String fruits = sc.next().toLowerCase();
 
         switch (fruits){
             case "apple" :
@@ -13,14 +13,26 @@ public class SelectingFruit {
             case "mango" :
                 System.out.println("Everyone's Favourite");
                 break;
-            case "Banana":
+            case "banana":
                 System.out.println("Makes Strong");
                 break;
-            case "Watermelon" :
+            case "watermelon" :
                 System.out.println("Summer Fruit");
                 break;
             default:
-                System.out.println("You have Write different fruit than me");
+                System.out.println("You have written different fruit than me");
         }
+
+// ------------------------------------------------------------------------------------------------------------------------ //
+
+        //      Enhanced switch, Here No need to apply break condition
+
+//        switch (fruits){
+//            case "apple" -> System.out.println("Doctor's Enemy");
+//            case "mango" -> System.out.println("Everyone's Favourite");
+//            case "banana" -> System.out.println("Makes Strong");
+//            case "watermelon" -> System.out.println("Summer Fruit");
+//            default -> System.out.println("You have written different fruit than me");
+//        }
     }
 }
